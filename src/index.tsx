745649a040f711ff03c2060327bd7d0683916557
Atharva Bhange <atharva.bhange@gmail.com>
2021-03-04 13:58:42 +0530

    starter project updated

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./App";
 import reducer from "reducer";
 
-const store = createStore(reducer, applyMiddleware());
+const store = createStore(reducer, composeWithDevTools(applyMiddleware()));
 
 ReactDOM.render(
 	<React.StrictMode>
